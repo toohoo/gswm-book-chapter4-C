c0f3709657243ed8183ed49968e20ad3e3e75739
Thomas Hofmann <toohoo@gmail.com>
2018-11-09 11:28:30 +0100

    first add files

---
+++ README.md
@@ -0,0 +1,4 @@
+# gswm-book-chapter4
+introducing-maven-ch4
+
+only the included code samples from the book - in this case chapter 4
---
+++ gswm/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		sayHello();
+	}
+	public static void sayHello() {
+		System.out.print("Hello World");
+	}
+}
